aae6485b3b35df51f9a84de3a7cc7e73f93964eb
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-04-24 23:23:15 +0300

    Add promiseAll

--- app1.js
+++ app1.js
@@ -9,19 +9,64 @@
 // }
 // PARALLEL REQUESTS!
 
+// async function get3Pokemon() {
+//     const prom1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
+//     const prom2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
+//     const prom3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
+//     console.log(prom1)
+//     const poke1 = await prom1;
+//     const poke2 = await prom2;
+//     const poke3 = await prom3;
+
+//     console.log(poke1.data);
+//     console.log(poke2.data);
+//     console.log(poke3.data);
+// }
+
+///PROMISE ALL
+
 async function get3Pokemon() {
     const prom1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
     const prom2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
     const prom3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
-    console.log(prom1)
-    const poke1 = await prom1;
-    const poke2 = await prom2;
-    const poke3 = await prom3;
-
-    console.log(poke1.data);
-    console.log(poke2.data);
-    console.log(poke3.data);
+    const results = await Promise.all([prom1, prom2, prom3]);
+    printPokemon(results);
 }
 
-
 get3Pokemon()
+
+function printPokemon(results) {
+    for (let pokemon of results) {
+        console.log(pokemon.data.name);
+    }
+}
+// function changeBodyColor(color, delay) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         }, delay)
+//     });
+// }
+
+// async function lightShow() {
+//     await changeBodyColor("teal", 1000);
+//     await changeBodyColor("red", 1000);
+//     await changeBodyColor("yellow", 1000);
+//     await changeBodyColor("violet", 1000);
+// }
+
+// save time, parallel
+
+// async function lightShow() {
+//     const p1 = changeBodyColor("teal", 1000);
+//     const p2 = changeBodyColor("red", 1000);
+//     const p3 = changeBodyColor("yellow", 1000);
+//     const p4 = changeBodyColor("violet", 1000);
+//     await p1;
+//     await p2;
+//     await p3;
+//     await p4;
+// }
+
+// lightShow();
